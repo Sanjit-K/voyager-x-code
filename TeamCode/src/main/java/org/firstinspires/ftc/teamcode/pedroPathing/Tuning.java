ce345c932e0599d9de4443b45bd0a7d67213bd10
xedrft <130224685+xedrft@users.noreply.github.com>
2025-09-21 15:20:13 -0500

    Fixed some constants and tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -78,10 +78,10 @@ public class Tuning extends SelectableOpMode {
     @Override
     public void onSelect() {
         if (follower == null) {
-            follower = org.firstinspires.ftc.teamcode.pedroPathing.constants.Constants.createFollower(hardwareMap);
+            follower = Constants.createFollower(hardwareMap);
             PanelsConfigurables.INSTANCE.refreshClass(this);
         } else {
-            follower = org.firstinspires.ftc.teamcode.pedroPathing.constants.Constants.createFollower(hardwareMap);
+            follower = Constants.createFollower(hardwareMap);
         }
 
         follower.setStartingPose(new Pose());
