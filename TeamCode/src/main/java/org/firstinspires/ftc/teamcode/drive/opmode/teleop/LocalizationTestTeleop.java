1c2471654dcdd61537b3a7fdca7f66761dbb3d0e
xedrft <130224685+xedrft@users.noreply.github.com>
2026-01-09 19:29:42 -0600

    turret tracking

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/LocalizationTestTeleop.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.teleop;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.FTCCoordinates;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@TeleOp
+public class LocalizationTestTeleop extends OpMode {
+    private Limelight3A limelight; //any camera here
+    private Follower follower;
+    private boolean following = false;
+    private final Pose TARGET_LOCATION = new Pose(72,72, 180); //Put the target location here
+
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        limelight.start();
+        limelight.pipelineSwitch(1); // Switch to pipeline number 1
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose()); //set your starting pose
+    }
+
+    @Override
+    public void loop() {
+
+        follower.update();
+
+        //if you're not using limelight you can follow the same steps: build an offset pose, put your heading offset, and generate a path etc
+
+        if (!following) {
+            follower.followPath(
+                    follower.pathBuilder()
+                            .addPath(new BezierLine(follower.getPose(), TARGET_LOCATION))
+                            .setLinearHeadingInterpolation(follower.getHeading(), TARGET_LOCATION.minus(follower.getPose()).getAsVector().getTheta())
+                            .build()
+            );
+        }
+
+        //This uses the aprilTag to relocalize your robot
+        //You can also create a custom AprilTag fusion Localizer for the follower if you want to use this by default for all your autos
+        follower.setPose(getRobotPoseFromCamera());
+
+        if (following && !follower.isBusy()) following = false;
+    }
+
+    private Pose getRobotPoseFromCamera() {
+        //Fill this out to get the robot Pose from the camera's output (apply any filters if you need to using follower.getPose() for fusion)
+        //Pedro Pathing has built-in KalmanFilter and LowPassFilter classes you can use for this
+        // First, tell Limelight which way your robot is facing
+        LLResult result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            Pose3D botpose = result.getBotpose();
+            if (botpose != null) {
+                double x = botpose.getPosition().x;
+                double y = botpose.getPosition().y;
+                telemetry.addData("MT1 Location", "(" + x + ", " + y + ")");
+                return new Pose(x, y, follower.getHeading(), FTCCoordinates.INSTANCE).getAsCoordinateSystem(PedroCoordinates.INSTANCE);
+            }
+        }
+        return follower.getPose(); //return current pose if no valid camera data
+
+    }
+}
